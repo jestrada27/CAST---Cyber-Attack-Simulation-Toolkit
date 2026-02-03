065838b126ff4e4479c0e266a65ed8da4d363175
NoahFCode <mail2noahf@gmail.com>
2026-02-03 11:36:53 -0800

    Create database.py

---
+++ database.py
@@ -0,0 +1,11 @@
+
+from pymongo import MongoClient
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+connection = os.getenv('MONGODB_URI')
+
+dbclient = MongoClient(connection, tlsAllowInvalidCertificates=True)  
+
+database_name = dbclient["CAST"]
