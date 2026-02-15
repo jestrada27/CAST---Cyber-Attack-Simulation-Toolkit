3219372b7de175d2edb5a86ab538aa146fbf119b
NoahFCode <mail2noahf@gmail.com>
2026-02-14 20:31:12 -0800

    Update routes.py

--- reports/routes.py
+++ reports/routes.py
@@ -5,12 +5,12 @@ from .reports_db import (getReportsForUser, get_filtered_logs, serialize_attack_
 #reports_bp = Blueprint("reports", __name__, url_prefix="/reports")
 reports_bp = Blueprint("reports", __name__)
 
-
+#route and function for getting the user report
 @reports_bp.route("/user_report", methods=["GET"])
 def get_user_report():
    if "user_id" not in session:
        return {"success": False, "message": "Not logged in"}, 401
-   
+   #based on user_id and gets the reports that are tied to the user
    user_id = session["user_id"]
    user_report = getReportsForUser(user_id)
    
@@ -18,18 +18,18 @@ def get_user_report():
       return jsonify({"success": True, "attacks": [], "message": "No attack logs/reports have been generated yet."})
 
    #user_report = [serialize_attack_log(report) for report in user_report]
+   #formats the reports correctly
    user_report = [serialize(report) for report in user_report]
 
    return jsonify({"success": True, "attacks": user_report})
 
-#@reports_bp #route for serialize attack log?
 
-
+#route to get the attack logs based on the function. it gets the list of attack logs that are in the db and tied to user
 @reports_bp.route("/attack_logs", methods=["GET"])
 def get_attack_logs():
    if "user_id" not in session:
        return {"success": False, "message": "Not logged in"}, 401
-  
+  #gets all logs for the user
    user_id = session['user_id']
    attack_logs = get_all_logs(user_id)
    
@@ -37,73 +37,75 @@ def get_attack_logs():
       return jsonify({"success": False, "attacks": [], "message": "No reports have been generated yet."})
    
    #attack_logs = [serialize_attack_log(attack) for attack in attack_logs]
-
+   #returns the logs 
    return jsonify({"success": True, "attacks": attack_logs})
 
-
+#route to get the stats about attacks
 @reports_bp.route("/attack_stats", methods=["GET"])
 def get_stats():
    if "user_id" not in session:
        return {"success": False, "message": "Not logged in"}, 401
-   
+   #gets user id and uses function to get the stats and returns stats
    user_id = session['user_id']
    attack_stats = get_attack_stats(user_id)
    return jsonify({"success": True, "stats": attack_stats})
    
 
-
+#route for deleting an attack when the user wants to delete a specific attack
 @reports_bp.route("/delete_attack/<attack_id>", methods=["DELETE"])
 def delete_route(attack_id):
    if "user_id" not in session:
        return {"success": False, "message": "Not logged in"}, 401
-   
+   #userid is used to go to user and delete function is used to delete the specific attack and then it's returned
    user_id = session["user_id"]
    attack_deleted = db_delete(attack_id, user_id)
    
    return {"success": True, "deleted":attack_deleted, "message": "Attack deleted."}
 
-
+#route and function for clearing all the list of attack logs that are tied to the user.
 @reports_bp.route("/clear_all_attacks", methods=["DELETE"])
 def clear_all_route():
     if "user_id" not in session:
        return {"success": False, "message": "Not logged in"}, 401
-    
+    #uses userid for user and then uses the clear function to clear all of the attacks. 
     user_id = session['user_id']
 
     cleared_attacks = db_clear(user_id)
     return {"success": True, "deleted": cleared_attacks, "message": "Cleared."}
 
-
+#database route so the database page
 @reports_bp.route("/database")
 def database_page():
    if "user_id" not in session:
        return {"success": False, "message": "Not logged in"}, 401
-   
+   #sets up the database page by getting all logs and attack stats that are shown on the page and renders it
    user_id = session["user_id"]
    logs = get_all_logs(user_id)
    stats = get_attack_stats(user_id)
 
    return render_template("database.html", logs=logs, stats=stats, username=session["username"])
 
-
+#route and function for the user to be able to filter and sort the attacks in their list.
 @reports_bp.route("/attack_logs/filter", methods=["GET"])
 def filter_attack_log():
    if "user_id" not in session:
        return {"success": False, "message": "Not logged in"}, 401
     
+   #gets information for the sorting and the filtering for when the route is used
    user_id = session["user_id"]
    attack_type = request.args.get("attack_type")
    status = request.args.get("status")
    performance = request.args.get("performance")
    sorter = request.args.get("sorter", "Newest")
   
+   #uses the information to filter and sort by using the function and then returning filtered logs
    filtered_logs = get_filtered_logs(
       user_id, attack_type=attack_type, status=status,
       performance=performance, sorter=sorter)
 
    return jsonify({"success": True, "attacks": filtered_logs})
 
-
+#testing route for the simulate / generate attack to populate db so we can get attack results/reports showing
 @reports_bp.route("/simulate_attack", methods=["POST"])
 def simulate_attack():
 
